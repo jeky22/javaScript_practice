a90a5cdad159b66288357c1abc692e4840dcaa5d
jeky22 <qhzh33@gmail.com>
2019-07-23 11:15:08 +0900

    structure commit

--- js/structure.js
+++ js/structure.js
@@ -5,7 +5,19 @@ function myFunction(p1, p2) {
 document.getElementById("demo01").innerHTML = myFunction(4, 3);
 
 function toCelsius(fahrenheit) {
-    return (5/9) * (fahrenheit - 32);
+    return (5 / 9) * (fahrenheit - 32);
 }
 
 document.getElementById("demo02").innerHTML = toCelsius(77) + " Celsius";
+
+function concatenate(first, last) {
+    var full;
+    full = first + last;
+    return full;
+}
+
+function secondFunction() {
+    var result;
+    result = concatenate('Zara', 'Ali');
+    document.getElementById("demo03").innerHTML=result;
+}

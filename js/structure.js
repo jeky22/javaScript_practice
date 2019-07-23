ac0984d8d57a32b4adf5c15e4bf3fe9065ee7ff1
Hyun Yu Jin <@naver.com>
2019-07-23 11:12:29 +0900

    structure2

--- js/structure.js
+++ js/structure.js
@@ -1,3 +1,4 @@
+// Structure
 function myFunction(p1, p2) {
     return p1 * p2;
 }
@@ -9,3 +10,16 @@ function toCelsius(fahrenheit) {
 }
 
 document.getElementById("demo02").innerHTML = toCelsius(77) + " Celsius";
+
+// Call with function
+function concatenate(first, last) {
+    var full;
+    full = first + last;
+    return full;
+}
+
+function secondFunction() {
+    var result;
+    result = concatenate('Zara', 'Ali');
+    document.write(result);
+}

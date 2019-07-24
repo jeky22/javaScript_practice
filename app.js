60909e2d86eeb390cfbaa74aae64cc51705c1dcc
Hyun Yu Jin <@naver.com>
2019-07-24 17:48:49 +0900

    c

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-app.use(express.static('public'))
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(express.static('public')) // 정적인 파일 만들기 - 여기에 해당하는 폴더 이름을 써준다.
 
 const path = require('path');
 const router = express.Router();
-
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
     //__dirname : It will resolve to your project folder.
@@ -10,6 +15,11 @@ router.get('/', function (req, res) {
 
 router.get('/yj_eventTry', function (req, res) {
     res.sendFile(path.join(__dirname + '/yj_eventTry.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/cookies', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/cookies.html'));
     //__dirname : It will resolve to your project folder.
 });
 

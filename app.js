90dcda525a2b1a2dd0cfe40ef942ccc2dd2a2bd4
jeky22 <qhzh33@gmail.com>
2019-07-26 11:36:22 +0900

    bef merge

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ router.get('/jc_cookieTry', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/jc_cookieTry.html'));
     //__dirname : It will resolve to your project folder.
 });
-
+router.get('/jc_jqueryTry', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/jc_jqueryTry.html'));
+    //__dirname : It will resolve to your project folder.
+});
 
 router.get('/join', function (req, res) {
     console.log(req.query);
